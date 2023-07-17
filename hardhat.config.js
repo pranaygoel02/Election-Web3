96259971558db62316591b41a8a0c15f6d800c2f
Pranay Goel <goel.2002pranay@gmail.com>
2023-07-17 22:38:42 +0530

    smart contract

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+require("@matterlabs/hardhat-zksync-solc");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "polygon_mumbai",
+  networks: {
+    hardhat: {
+      chainId: 80001,
+    },
+    polygon_mumbai: {
+      url: process.env.MUMBAI_RPC_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
+  solidity: {
+    version: "0.8.7",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
